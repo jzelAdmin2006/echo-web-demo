cd1e11754f4cd876ba9564a5a0032c5bcb4a1d84
Johannes Zeller <jo.zeller@protonmail.com>
2024-02-01 18:08:45 +0100

    implement plus endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,10 +11,32 @@ type Message struct {
 	Message string `json:"message"`
 }
 
+type Result struct {
+	Result int `json:"result"`
+}
+
+type Error struct {
+	Error string `json:"error"`
+}
+
+func Plus() func(c echo.Context) error {
+	return func(c echo.Context) error {
+		a, err := strconv.Atoi(c.QueryParam("a"))
+		b, err := strconv.Atoi(c.QueryParam("b"))
+		if err != nil {
+			return c.JSON(400, Error{
+				Error: "invalid parameter",
+			})
+		}
+		return c.JSON(http.StatusOK, Result{Result: a + b})
+	}
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, Message{Message: "pong"})
 	})
+	e.GET("/plus", Plus())
 	e.Logger.Fatal(e.Start(":8080"))
 }
